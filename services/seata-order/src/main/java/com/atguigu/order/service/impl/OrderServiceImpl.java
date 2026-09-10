12c5a8d7ecf48c91a73daf9b87689d60ce24b83b
克鲁鲁 <3418001848@qq.com>
2026-09-10 20:09:53 +0800

    seata实现分布式事务

--- services/seata-order/src/main/java/com/atguigu/order/service/impl/OrderServiceImpl.java
+++ services/seata-order/src/main/java/com/atguigu/order/service/impl/OrderServiceImpl.java
@@ -1,24 +1,29 @@
 package com.atguigu.order.service.impl;
 
 import com.atguigu.order.bean.OrderTbl;
+import com.atguigu.order.feign.AccountFeignClient;
 import com.atguigu.order.mapper.OrderTblMapper;
 import com.atguigu.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     @Autowired
     OrderTblMapper orderTblMapper;
+    @Autowired
+    private AccountFeignClient accountFeignClient;
 
+    @Transactional
     @Override
     public OrderTbl create(String userId, String commodityCode, int orderCount) {
         //1、计算订单价格
         int orderMoney = calculate(commodityCode, orderCount);
 
         //TODO 2、扣减账户余额
-
+        accountFeignClient.debit(userId, orderMoney);
         //3、保存订单
         OrderTbl orderTbl = new OrderTbl();
         orderTbl.setUserId(userId);
@@ -27,6 +32,7 @@ public class OrderServiceImpl implements OrderService {
         orderTbl.setMoney(orderMoney);
 
         orderTblMapper.insert(orderTbl);
+        int i=10/0;
 
         return orderTbl;
     }
--- services/seata-storage/src/main/java/com/atguigu/storage/controller/StorageRestController.java
+++ services/seata-storage/src/main/java/com/atguigu/storage/controller/StorageRestController.java
@@ -3,6 +3,7 @@ package com.atguigu.storage.controller;
 
 import com.atguigu.storage.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
